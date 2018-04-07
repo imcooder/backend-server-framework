c2d1b57de6f71e0193685ca311c1ebac745abafb
xuejuntao <xuejuntao@baidu.com>
2018-04-07 20:27:05 +0800

    update

--- index.js
+++ index.js
@@ -27,78 +27,80 @@ function json(status, msg, data) {
     }
     return ret;
 }
-var BackendService = function(opt) {
-    console.log("[backend_server]new:%j", opt);
-    let self = this;
-    self.opt = {
-        crossOptions: null,
-    };
-    if (opt) {
-        self.opt = _.extend(self.opt, opt);
+class BackendService {
+    constructor(opt) {
+        console.log("[backend_server]new:%j", opt);
+        let self = this;
+        self.opt = {
+            crossOptions: null,
+        };
+        if (opt) {
+            self.opt = _.extend(self.opt, opt);
+        }
+        self.express = express();
     }
-    self.express = null;
-};
-BackendService.prototype.start = function() {
-    console.info("[backend_server]start");
-    let self = this;
-    self.express = express();
-    self.express.use(bodyParser.json({
-        limit: '30mb'
-    }));
-    self.express.use(bodyParser.urlencoded({
-        extended: true
-    }));
-    self.express.use(cookieParser());
-    if (self.opt.crossOptions !== null) {
-        self.express.use(cors(self.opt.crossOptions));
-    }
-    if (_.isArray(self.opt.router)) {
-        self.opt.router.forEach(function(item) {
-            loadRouter(self.express, item.router, item.path);
+    start() {
+        console.info("[backend_server]start");
+        let self = this;
+        self.express.use(bodyParser.json({
+            limit: '30mb'
+        }));
+        self.express.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        self.express.use(cookieParser());
+        if (self.opt.crossOptions !== null) {
+            self.express.use(cors(self.opt.crossOptions));
+        }
+        if (_.isArray(self.opt.router)) {
+            self.opt.router.forEach(function (item) {
+                loadRouter(self.express, item.router, item.path);
+            });
+        }
+        self.express.get('/', function (req, res) {
+            res.send('hello world');
         });
-    }
-    self.express.get('/', function(req, res) {
-        res.send('hello world');
-    });
-    self.express.on('error', (error) => {
-        console.error('express:error:%s', error.stack);
-    });
-    self.express.on('uncaughtException', (error) => {
-        console.error('express::uncaughtException:', error);
-        console.error('express:uncaughtException:%s', error.stack);
-        return;
-    });
-    let p = new Promise(function(resolve, reject) {
-        let port = self.opt.port || 8000;
-        console.log('[backend_server]listen:%d', port);
-        self.server = self.express.listen(port, function(error) {
-            console.log('http callback:', error);
-            if (_.has(self.opt, 'timeout')) {
-                self.server.timeout = self.opt.timeout;
-            }
-            resolve();
+        self.express.on('error', (error) => {
+            console.error('express:error:%s', error.stack);
+        });
+        self.express.on('uncaughtException', (error) => {
+            console.error('express::uncaughtException:', error);
+            console.error('express:uncaughtException:%s', error.stack);
             return;
-        }).on('error', function(error) {
-            console.error('express listen error:%s', error.stack);
-            if (error.code === 'EADDRINUSE') {
-                reject(error);
+        });
+        let p = new Promise(function (resolve, reject) {
+            let port = self.opt.port || 8000;
+            console.log('[backend_server]listen:%d', port);
+            self.server = self.express.listen(port, function (error) {
+                console.log('http callback:', error);
+                if (_.has(self.opt, 'timeout')) {
+                    self.server.timeout = self.opt.timeout;
+                }
+                resolve();
                 return;
-            }
-            resolve();
-            return;
+            }).on('error', function (error) {
+                console.error('express listen error:%s', error.stack);
+                if (error.code === 'EADDRINUSE') {
+                    reject(error);
+                    return;
+                }
+                resolve();
+                return;
+            });
         });
-    });
-    return p;
-};
-BackendService.prototype.stop = function() {
-    console.log('[backend_server]stop');
-    let self = this;
-    if (self.server) {
-        self.server.close();
-        self.server = null;
+        return p;
     }
-};
-module.exports.createServer = function(opt) {
+    stop() {
+        console.log('[backend_server]stop');
+        let self = this;
+        if (self.server) {
+            self.server.close();
+            self.server = null;
+        }
+    }
+}
+
+module.exports.createServer = function (opt) {
     let server = new BackendService(opt);
     return server;
 };
